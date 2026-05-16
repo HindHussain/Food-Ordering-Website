8e0a3c30d2da8cf83c1a5b6b48416a25fc4dffa0
HindHussain <hindoo1760@gmail.com>
2026-05-16 20:30:43 +0300

    Delete Food-Ordering-System/app.js

--- Food-Ordering-System/app.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll(".add-btn");
-
-buttons.forEach(button => {
-    button.addEventListener("click", function () {
-        const item = this.getAttribute("data-item");
-        alert(item + " added to cart!");
-    });
-});
